4b2d0e1e279e79fdaa7ac7802abcacb3dea1cc27
uchkun <qorakissa33@gmail.com>
2022-05-23 09:08:13 +0500

    saboq v1

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-document.getElementById('title');
-console.log(document.getElementById('title'));
+let dom = document.getElementById('title');
+console.log(dom.id);
+console.log(dom.className);
+
+const listItem = document.querySelectorAll('li');
+
+console.log(listItem);
+
+listItem.forEach(function (items) {
+    items.CDATA_SECTION_NODE;
+})
