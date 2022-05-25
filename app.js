b8e44bfd467d69b8ed95277129994a07a2a0378b
uchkun <qorakissa33@gmail.com>
2022-05-25 07:14:34 +0500

    Second

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-let dom = document.getElementById('title');
-console.log(dom.id);
-console.log(dom.className);
+// let dom = document.getElementById('title');
+// console.log(dom.id);
+// console.log(dom.className);
 
-const listItem = document.querySelectorAll('li');
+// const listItem = document.querySelectorAll('li');
 
-console.log(listItem);
-console.log(listItem[0]);
-console.log(listItem[0].textContent);
-console.log(listItem[3].textContent);
-console.log(listItem[2].style.color = 'red')
-console.log(listItem[1].innerHTML = '<span></span>');
-console.log(listItem[1].style.display = 'none');
+// console.log(listItem);
+// console.log(listItem[0]);
+// console.log(listItem[0].textContent);
+// console.log(listItem[3].textContent);
+// console.log(listItem[2].style.color = 'red')
+// console.log(listItem[1].innerHTML = '<span></span>');
+// console.log(listItem[1].style.display = 'none');
+
+let val;
+val = 1;
+console.log(val);
