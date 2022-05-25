2d0f36cb01ebf8ceadc49a88eb9cd2fa5913db24
uchkun <qorakissa33@gmail.com>
2022-05-25 10:38:12 +0500

    nuberv1

--- app.js
+++ app.js
@@ -12,6 +12,26 @@
 // console.log(listItem[1].innerHTML = '<span></span>');
 // console.log(listItem[1].style.display = 'none');
 
-let val;
-val = 1;
-console.log(val);
+// let val;
+// val = 6;
+// console.log(val);
+
+const Person = function (name, birthYaer) {
+    this.name = name;
+    this.birthYaer = birthYaer;
+    // this.clac = function(){
+    //     console.log(2022-birthYaer);
+    // };
+}
+
+
+const uchkun = new Person('Uchkun', 1987);
+const temur = new Person('Temur', 2014)
+
+console.log(Person.prototype)
+Person.prototype.calcAge = function(){
+    console.log(2022-this.birthYaer);
+};
+
+uchkun.calcAge()
+temur.calcAge()
